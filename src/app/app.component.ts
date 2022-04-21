749092df5ff9f975acc2a072b9267747acf0eb8a
Denis SOURON <denis.souron.externe@capgemini.com>
2022-04-21 16:41:17 +0200

    install angular2-baidu-map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MapOptions } from 'angular2-baidu-map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-baidu-strict';
+  options: MapOptions;
+
+  constructor() {
+    this.options = {
+      centerAndZoom: {
+        lat: 39.920116,
+        lng: 116.403703,
+        zoom: 16,
+      },
+      enableKeyboard: true,
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BaiduMapModule } from 'angular2-baidu-map';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BaiduMapModule.forRoot({ ak: 'your ak' })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
